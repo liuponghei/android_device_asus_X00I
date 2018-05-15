22947b23b4c91cb2eaca9b04a9ab70a89ecd304f
liuponghei <liuponghei@users.noreply.github.com>
2018-05-16 02:28:11 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,8 +35,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000
 
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
-#TARGET_KERNEL_CONFIG := msm8937-perf_defconfig
-TARGET_PREBUILT_KERNEL := device/asus/X00I/kernel
+TARGET_KERNEL_CONFIG := msm8937-perf_defconfig
 
 TARGET_LDPRELOAD := libNimsWrap.so
 
