1748e03b1eab4fe7bba616e07c303c3a5c522ea8
liuponghei <liuponghei@users.noreply.github.com>
2018-05-16 07:30:32 +0800

    Update omni_X00I.mk

--- omni_X00I.mk
+++ omni_X00I.mk
@@ -6,3 +6,8 @@ PRODUCT_DEVICE := X00I
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := ASUS_X00IDB
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+TARGET_BOOTANIMATION_SIZE := 1920x1080
+$(call inherit-product, vendor/omni/config/common.mk)
